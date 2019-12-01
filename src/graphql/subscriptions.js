e62cf9f0e4875dc68141f02307fe7463ed3bb9a8
Ricardo Nunes <sanunes.ricardo@gmail.com>
2019-12-01 15:54:55 -0500

    INIT | Initial commit

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    clientId
+    name
+    description
+    completed
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    clientId
+    name
+    description
+    completed
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    clientId
+    name
+    description
+    completed
+  }
+}
+`;
