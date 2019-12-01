7a24881ebbbe2d60109b679a7e262e5bcd28c378
Ricardo Nunes <sanunes.ricardo@gmail.com>
2019-12-01 17:28:15 -0500

    AUTH | Added Nav component

---
+++ src/Nav.js
@@ -0,0 +1,31 @@
+/* src/Nav.js */
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Menu, Icon } from 'antd'
+
+const Nav = (props) => {
+  const { current } = props
+  return (
+    <div>
+      <Menu selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key='home'>
+          <Link to={`/`}>
+            <Icon type='home' />Home
+          </Link>
+        </Menu.Item>
+        <Menu.Item key='profile'>
+          <Link to={`/profile`}>
+            <Icon type='profile' />Profile
+          </Link>
+        </Menu.Item>
+        <Menu.Item key='protected'>
+          <Link to={`/todos`}>
+            <Icon type='protected' />Todos
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </div>
+  )
+}
+
+export default Nav
